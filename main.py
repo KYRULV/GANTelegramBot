274974bca47823a9983541eac0414db283a4f815
unknokable <146101999+unknokable@users.noreply.github.com>
2024-05-17 18:46:31 +0200

    add start command

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import api_key_reader
 import telebot
 
 bot = telebot.TeleBot(api_key_reader.get())
+
+@bot.message_handler(commands=["start"]) 
+def start(message):
+    print("hello")
+    bot.send_message(message.chat.id, "Привет, я телеграмм бот который загадывает числа!")
+    
+bot.infinity_polling() 
