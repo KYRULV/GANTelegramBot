e7d0dbeefcd699f9607aa92b1dce929a04680692
unknokable <146101999+unknokable@users.noreply.github.com>
2024-05-24 18:04:22 +0200

    Add number wishing functionality

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import api_key_reader
 import telebot
+import Player
+import random
 
 bot = telebot.TeleBot(api_key_reader.get())
 
+players = dict()
+
+def wish_a_number():
+    random_number = random.randint(0, 20)
+    return random_number
+
 @bot.message_handler(commands=["start"]) 
 def start(message):
-    print("hello")
-    bot.send_message(message.chat.id, "Привет, я телеграмм бот который загадывает числа!")
-    
-bot.infinity_polling() 
+    cur_player_id = message.from_user.id
+    players[cur_player_id] = Player.Player()
+    players[cur_player_id].wished_number = wish_a_number()
+    bot.send_message(message.chat.id, "Привет, я телеграмм бот который загадывает числа")
+    bot.send_message(message.chat.id, players[cur_player_id].wished_number)
+
+bot.infinity_polling()
